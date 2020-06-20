85e0d19cb0c19145eec0c8316464c21c97e9a248
Cho-heejung <dddd6368@naver.com>
2020-06-20 14:24:47 +0900

    issue #6

--- calc/fig.py
+++ calc/fig.py
@@ -1,6 +1,4 @@
 from cgi import parse_qs
-import matplotlib as mpl
-mpl.use('Agg')
 import matplotlib.pyplot as plt
 from template import html
 
@@ -8,13 +6,10 @@ def application(environ, start_response):
     d = parse_qs(environ['QUERY_STRING'])
     a = d.get('a', [''])[0]
     b = d.get('b', [''])[0]
-    sum = d.get('sum', [''])[0]
-    product = d.get('product', [''])[0]
     if '' not in [a, b]:
         a, b = int(a), int(b)
         sum = [a + b]
         product = [a * b]
-        fig = plt.figure()
 
     response_body = html % {
         'sum' : sum,
